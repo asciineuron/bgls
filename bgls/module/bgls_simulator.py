c37d5e4c7cdca95ef21bed2900e986de1cbc7bec
Alex Shapiro <alex_shapiro@outlook.com>
2022-10-31 13:37:41 +0100

    Correct bitstring to ndarray conversion

--- bgls/module/bgls_simulator.py
+++ bgls/module/bgls_simulator.py
@@ -17,13 +17,13 @@ class BglsSimulator(cirq.sim.SimulatesSamples):
         self._seed = seed
 
     def _run(
-            self,
-            circuit: "cirq.AbstractCircuit",
-            param_resolver: "cirq.ParamResolver",
-            repetitions: int,
+        self,
+        circuit: "cirq.AbstractCircuit",
+        param_resolver: "cirq.ParamResolver",
+        repetitions: int,
     ) -> Dict[str, np.ndarray]:
         """See definition in `cirq.SimulatesSamples`."""
-        # second option in case empty:
+        # second or option in case empty
         param_resolver = param_resolver or cirq.ParamResolver({})
         resolved_circuit = cirq.resolve_parameters(circuit, param_resolver)
         cirq.sim.simulator.check_all_resolved(resolved_circuit)
@@ -31,21 +31,21 @@ class BglsSimulator(cirq.sim.SimulatesSamples):
         return self._ryan_sample_final_results(resolved_circuit, repetitions)
 
     def _ryan_apply(
-            self,
-            operation: cirq.Operation,
-            state: np.ndarray,
-            qubits: FrozenSet[cirq.Qid],
+        self,
+        operation: cirq.Operation,
+        state: np.ndarray,
+        qubits: FrozenSet[cirq.Qid],
     ) -> np.ndarray:
         unitary = cirq.unitary(
             cirq.Moment(
                 operation,
-                [cirq.I.on(q) for q in qubits - set(operation.qubits)]
+                [cirq.I.on(q) for q in qubits - set(operation.qubits)],
             )
         )
         return unitary @ state
 
     def _ryan_sample_final_results(
-            self, resolved_circuit: cirq.AbstractCircuit, repetitions: int
+        self, resolved_circuit: cirq.AbstractCircuit, repetitions: int
     ) -> Dict[str, np.ndarray]:
         rng = np.random.RandomState(self._seed)
         records: Dict[str, np.ndarray] = {}
@@ -62,15 +62,18 @@ class BglsSimulator(cirq.sim.SimulatesSamples):
             for op in resolved_circuit.all_operations():
                 # record and quit when we hit the measurement gate:
                 if isinstance(op.gate, cirq.MeasurementGate):
-                    print(op)
                     meas_key = op.gate.key
                     if meas_key not in records:
                         # TODO assume here we only have 1 appearance of
                         #  key... so won't work with general circuit
                         records[meas_key] = np.zeros(
-                            (repetitions, 1, len(qubits)))
-                    # TODO make this fit: np.fromstring(bitstring):
-                    records[meas_key][repetition, 0, :] = 0.1
+                            (repetitions, 1, len(qubits))
+                        )
+                    # convert bitstring to numpy array:
+                    records[meas_key][repetition, 0, :] = np.fromstring(
+                        bitstring,
+                        "u1",
+                    ) - ord("0")
                     break
 
                 # Determine the candidate bitstrings to sample.
@@ -78,8 +81,7 @@ class BglsSimulator(cirq.sim.SimulatesSamples):
                 candidates = list(
                     itertools.product(
                         *[
-                            ["0",
-                             "1"] if i in op_support  # Update bits on
+                            ["0", "1"] if i in op_support  # Update bits on
                             # operation
                             # support.
                             else [b]  # Fix bits not on operation support.
@@ -89,16 +91,22 @@ class BglsSimulator(cirq.sim.SimulatesSamples):
                 )
 
                 # Compute probability of each candidate bitstring.
-                state = self._ryan_apply(op, state,
-                                         qubits)  # TODO: Make this an input.
-                probs = [abs(state[int("".join(bits), 2)]) ** 2 for bits in
-                         candidates]
+                state = self._ryan_apply(
+                    op, state, qubits
+                )  # TODO: Make this an input.
+                probs = [
+                    abs(state[int("".join(bits), 2)]) ** 2
+                    for bits in candidates
+                ]
 
                 # Sample from the candidate bitstrings and update the
                 # bitstring.
                 bitstring = "".join(
-                    candidates[rng.choice(range(len(candidates)),
-                                          p=probs / sum(probs))]
+                    candidates[
+                        rng.choice(
+                            range(len(candidates)), p=probs / sum(probs)
+                        )
+                    ]
                 )
 
         return records
