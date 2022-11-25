39995ec803ec41cc86f91f30a337112c2783ba26
Alex <alex_shapiro@outlook.com>
2022-11-25 14:47:22 +0100

    Added utils file for getting amplitude from state vector.

---
+++ bgls/module/bgls_utils.py
@@ -0,0 +1,32 @@
+# helper functions to operate on cirq structures
+
+import cirq
+import numpy as np
+from typing import TypeVar
+
+State = TypeVar("State")
+Bitstring = TypeVar("Bitstring")
+
+
+def state_vector_bitstring_amplitude(
+        state_vector: cirq.sim.state_vector_simulation_state
+        .StateVectorSimulationState,
+        bitstring: Bitstring,
+) -> complex:
+    """
+    For a cirq StateVectorSimulationState,
+    returns the amplitude corresponding to a given bitstring
+    """
+    index = int(bitstring, 2)
+    vector = cirq.to_valid_state_vector(state_vector.target_tensor)
+    amplitude = np.abs(vector[index])
+    return amplitude
+
+
+def density_matrix_bitstring_probability(
+        state: cirq.sim.DensityMatrixSimulationState, bitstring: Bitstring
+) -> float:
+    index = int(bitstring, 2)
+    density_matrix = cirq.to_valid_density_matrix(state.target_tensor)
+    probability = 0  # tr (prod_m (sum_j p_j |psi_j><psi_j|))
+    return probability
