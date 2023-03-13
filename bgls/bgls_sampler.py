130aa70ca98e6743d5c48e52eeb5bd985991a45c
Ryan LaRose <rlarose@umich.edu>
2023-03-13 15:12:39 +0100

    Inherit from `cirq.SimulatesSampels`. Tests partially updated.

--- bgls/bgls_sampler.py
+++ bgls/bgls_sampler.py
@@ -1,75 +1,135 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Defines the BGLS Simulator."""
+
 import itertools
-import cirq
-import numpy as np
 from typing import TypeVar, Callable, Any, Dict, List
 
+import numpy as np
 
-class Simulator:
-    State = TypeVar("State")
+import cirq
 
+
+State = TypeVar("State")
+
+
+class Simulator(cirq.SimulatesSamples):
     def __init__(
         self,
-        initial_state: State,
-        compute_probability: Callable[[State, str], float],
-        apply_gate: Callable[
-            [cirq.Operation, State], Any
-        ] = cirq.protocols.act_on,  # type: ignore[assignment]
-    ):
-        self.initial_state = initial_state
-        self.compute_probability = compute_probability
-        self.apply_gate = apply_gate
+        initial_state: State | None,
+        apply_gate: Callable[[cirq.Operation, State], None]
+        | None,  # TODO: Decide on return value.
+        compute_probability: Callable[[State, str], float] | None,
+        seed: cirq.RANDOM_STATE_OR_SEED_LIKE = None,
+    ) -> None:
+        """Initialize a BGLS Simulator.
 
-    def sample(
+        Args:
+            initial_state: The initial state of the circuit (conventionally the
+                all |0⟩ state.) Note: Must be provided to indicate the type and
+                 how to apply gates, in accordance with `apply_gate` below.
+            apply_gate: Function which inputs an operation and state and
+                applies the operation to the state, updating the state in
+                place.
+            compute_probability: Function which inputs a state and bitstring
+                and returns the probability of the given bitstring.
+            seed: Seed or random state for sampling.
+        """
+        # TODO: These three parameters (initial_state, apply_gate,
+        #  and compute_probability) are not independent and should be handled
+        #  by a data class, e.g., `BglsOptions`.
+
+        # TODO: Add default options.
+        self._initial_state = initial_state
+        self._apply_gate = apply_gate
+        self._compute_probability = compute_probability
+
+        self._rng = cirq.value.parse_random_state(seed)
+
+    def _run(
         self,
-        circuit: cirq.Circuit,
-        param_resolver: "cirq.ParamResolverOrSimilarType" = None,
-        repetitions: int = 1,
-        seed: "cirq.RANDOM_STATE_OR_SEED_LIKE" = None,
-    ) -> cirq.Result:
-        rng = cirq.value.parse_random_state(seed)
+        circuit: "cirq.AbstractCircuit",
+        param_resolver: "cirq.ParamResolver",
+        repetitions: int,
+    ) -> Dict[str, np.ndarray]:
+        """Run a simulation, mimicking quantum hardware.
 
-        resolved_circuit = cirq.resolve_parameters(circuit, param_resolver)
-        cirq.sim.simulator.check_all_resolved(resolved_circuit)
+        Args:
+            circuit: The circuit to simulate.
+            param_resolver: Parameters to run with the program.
+            repetitions: Number of times to repeat the run.
 
+        Returns:
+            A dictionary from measurement gate key to measurement
+            results. Measurement results are stored in a 3-dimensional
+            numpy array, the first dimension corresponding to the
+            repetition, the second to the instance of that key in the
+            circuit, and the third to the actual boolean measurement
+            results (ordered by the qubits being measured.)
+        """
         if not circuit.are_any_measurements_terminal():
             raise ValueError("No terminal measurements to sample.")
 
-        records: Dict[str, np.ndarray] = {}
+        param_resolver = param_resolver or cirq.ParamResolver()
+        resolved_circuit = cirq.resolve_parameters(circuit, param_resolver)
 
+        return self._sample(resolved_circuit, repetitions)
+
+    def _sample(
+        self,
+        circuit: "cirq.AbstractCircuit",
+        repetitions: int = 1,
+    ) -> Dict[str, np.ndarray]:
+        records: Dict[str, np.ndarray] = {}
+        """Returns a number of measurements by simulating the circuit.
+        
+        Args:
+            circuit: The circuit to simulate.
+            repetitions: The number of times to simulate the circuit 
+                (number of measurements to return).
+        """
         for rep in range(repetitions):
-            keys_to_bitstrs = self.sample_core(
-                resolved_circuit,
-                rng=rng,
-            )
-            for meas_key in keys_to_bitstrs:
+            keys_to_bitstrings = self._sample_once(circuit)
+            for meas_key in keys_to_bitstrings:
                 if rep == 0 and meas_key not in records:
                     records[meas_key] = np.zeros(
-                        (repetitions, 1, len(keys_to_bitstrs[meas_key][-1]))
+                        (repetitions, 1, len(keys_to_bitstrings[meas_key][-1]))
                     )
                 records[meas_key][rep, 0, :] = [
-                    int(bit) for bit in keys_to_bitstrs[meas_key][-1]
+                    int(bit) for bit in keys_to_bitstrings[meas_key][-1]
                 ]
 
-        return cirq.study.ResultDict(records=records)
+        return records
 
-    def sample_core(
-        self,
-        circuit: cirq.Circuit,
-        rng: np.random.RandomState = cirq.value.parse_random_state(None),
-        return_history: bool = False,
+    def _sample_once(
+        self, circuit: "cirq.AbstractCircuit"
     ) -> Dict[str, List[str]]:
-        keys_to_indices: Dict[str, List[int]] = {}
-        keys_to_bitstrings: Dict[str, List[str]] = {}
+        """Returns one measurement by simulating the circuit.
 
+        Args:
+            circuit: The circuit to simulate.
+        """
         qubits = circuit.all_qubits()
         qubit_index = {q: i for i, q in enumerate(sorted(qubits))}
         bitstring = "0" * len(qubits)
         bitstrings = [bitstring]
 
-        state = self.initial_state.copy()
+        state = (
+            self._initial_state.copy()
+        )  # TODO: Update or require states to have copy method.
+        keys_to_indices: Dict[str, List[int]] = {}
         for i, op in enumerate(circuit.all_operations()):
             if cirq.protocols.is_measurement(op):
-                # check if terminal:
                 if circuit.next_moment_operating_on(op.qubits, i + 1) is None:
                     meas_key = cirq.protocols.measurement_key_name(op.gate)
                     if meas_key not in keys_to_indices:
@@ -77,9 +137,9 @@ class Simulator:
                         keys_to_indices[meas_key] = meas_indices
                 continue
 
-            self.apply_gate(op, state)
+            self._apply_gate(op, state)
 
-            # Determine the candidate bitstrings to sample:
+            # Determine the candidate bitstrings to sample.
             op_support = {qubit_index[q] for q in op.qubits}
             candidates = list(
                 itertools.product(
@@ -91,18 +151,18 @@ class Simulator:
             )
             joined_cands = ["".join(cand) for cand in candidates]
 
-            # Compute probability of each candidate bitstring:
+            # Compute probability of each candidate bitstring.
             candidate_probs = np.asarray(
                 [
-                    self.compute_probability(state, candidate)
+                    self._compute_probability(state, candidate)
                     for candidate in joined_cands
                 ]
             )
 
-            # sample to get bitstring
+            # Sample to get bitstring.
             bitstring = "".join(
                 candidates[
-                    rng.choice(
+                    self._rng.choice(
                         range(len(candidates)),
                         p=candidate_probs / sum(candidate_probs),
                     )
@@ -110,29 +170,16 @@ class Simulator:
             )
             bitstrings.append(bitstring)
 
-        # return dict of list of bitstrings measured per gate
-        # (optionally over time)
+        # Return dict of list of bitstrings measured per gate.
+        keys_to_bitstrings: Dict[str, List[str]] = {}
         for meas in keys_to_indices:
-            if not return_history:
-                keys_to_bitstrings[meas] = [
-                    "".join(
-                        [
-                            bit
-                            for i, bit in enumerate(bitstring)
-                            if i in keys_to_indices[meas]
-                        ]
-                    )
-                ]
-            else:
-                keys_to_bitstrings[meas] = [
-                    "".join(
-                        [
-                            bit
-                            for i, bit in enumerate(bitstr)
-                            if i in keys_to_indices[meas]
-                        ]
-                    )
-                    for bitstr in bitstrings
-                ]
-
+            keys_to_bitstrings[meas] = [
+                "".join(
+                    [
+                        bit
+                        for i, bit in enumerate(bitstring)
+                        if i in keys_to_indices[meas]
+                    ]
+                )
+            ]
         return keys_to_bitstrings
--- bgls/bgls_sampler_test.py
+++ bgls/bgls_sampler_test.py
@@ -1,188 +1,215 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for the BGLS Simulator."""
+
 import pytest
-import numpy as np
+
 import cirq
+
 import bgls
 
-"""
-Test suite for proper behavior with seeds, measurements, and state types.
-"""
 
+@pytest.mark.parametrize("nqubits", range(3, 8 + 1))
+def test_samples_correct_bitstrings_for_ghz_circuit(nqubits: int):
+    """Tests correct measurement results for a GHZ circuit (should only return
+    the all 0 or all 1 bitstring).
 
-def test_seed_continuity():
-    # Running our sample function with the same seed should produce
-    # identical measurements
-    q0, q1, q2 = cirq.LineQubit.range(3)
-    ghz = cirq.Circuit(
-        cirq.H(q0),
-        cirq.CNOT(q0, q1),
-        cirq.CNOT(q0, q2),
-        cirq.measure([q0, q2], key="result1st"),
-    )
-    init_state = cirq.StateVectorSimulationState(
-        qubits=(q0, q1, q2), initial_state=0
+    Args:
+        nqubits: Number of qubits in GHZ circuit.
+    """
+    qubits = cirq.LineQubit.range(nqubits)
+    circuit = cirq.Circuit(
+        cirq.H.on(qubits[0]),
+        (cirq.CNOT.on(qubits[i], qubits[i + 1]) for i in range(nqubits - 1)),
+        cirq.measure(qubits, key="z"),
     )
 
-    bgls_simulator = bgls.Simulator(
-        init_state,
-        bgls.state_vector_bitstring_probability,
+    sim = bgls.Simulator(
+        initial_state=cirq.StateVectorSimulationState(
+            qubits=qubits, initial_state=0
+        ),
+        apply_gate=cirq.protocols.act_on,
+        compute_probability=bgls.state_vector_bitstring_probability,
+    )
+    results = sim.run(circuit, repetitions=100)
+    measurements = set(results.histogram(key="z").keys())
+    assert measurements.issubset({0, 2**nqubits - 1})
+
+
+def test_results_same_when_seeded():
+    """Tests simulator results are the same when provided the same seed."""
+    q = cirq.LineQubit(0)
+    circuit = cirq.Circuit(cirq.H.on(q), cirq.measure(q, key="z"))
+
+    sim_params = (
+        cirq.StateVectorSimulationState(qubits=(q,), initial_state=0),
         cirq.protocols.act_on,
-    )
-    bgls_result_1 = bgls_simulator.sample(
-        ghz,
-        repetitions=100,
-        seed=1,
-    )
-    bgls_result_2 = bgls_simulator.sample(
-        ghz,
-        repetitions=100,
-        seed=1,
-    )
-    assert bgls_result_1 == bgls_result_2
-
-
-def test_intermediate_measurements():
-    # Intermediate measurement gates do not affect simulation
-    q0, q1, q2 = cirq.LineQubit.range(3)
-    ghz = cirq.Circuit(
-        cirq.H(q0),
-        cirq.CNOT(q0, q1),
-        cirq.CNOT(q1, q2),
-        cirq.measure([q0, q1, q2], key="result"),
-    )
-    ghz_intermediate = cirq.Circuit(
-        cirq.H(q0),
-        cirq.measure([q0, q2], key="result"),
-        cirq.CNOT(q0, q1),
-        cirq.measure([q0, q1, q2], key="result"),
-        cirq.CNOT(q1, q2),
-        cirq.measure([q0, q1, q2], key="result"),
-    )
-    init_state = cirq.StateVectorSimulationState(
-        qubits=(q0, q1, q2), initial_state=0
-    )
-    bgls_simulator = bgls.Simulator(
-        init_state,
         bgls.state_vector_bitstring_probability,
-        cirq.protocols.act_on,
     )
-    bgls_result = bgls_simulator.sample(ghz, repetitions=100, seed=1)
-    bgls_result_intermediate = bgls_simulator.sample(
-        ghz_intermediate, repetitions=100, seed=1
-    )
-    assert bgls_result == bgls_result_intermediate
+    sim1 = bgls.Simulator(*sim_params, seed=1)
+    sim2 = bgls.Simulator(*sim_params, seed=1)
+
+    result1 = sim1.run(circuit, repetitions=100)
+    result2 = sim2.run(circuit, repetitions=100)
+
+    assert result1 == result2
 
 
-def test_multiple_measurements():
-    # Distributing final measurement across multiple gates has same behavior
-    # as a single measurement of all qubits
-    q0, q1, q2 = cirq.LineQubit.range(3)
-    ghz = cirq.Circuit(
-        cirq.H(q0),
-        cirq.CNOT(q0, q1),
-        cirq.CNOT(q0, q2),
-        cirq.measure([q0, q1, q2], key="result"),
-    )
-    ghz_multiple = cirq.Circuit(
-        cirq.H(q0),
-        cirq.CNOT(q0, q1),
-        cirq.CNOT(q0, q2),
-        cirq.measure([q0, q2], key="result1st"),
-        cirq.measure([q1], key="result2nd"),
-    )
-    init_state = cirq.StateVectorSimulationState(
-        qubits=(q0, q1, q2), initial_state=0
-    )
-    bgls_simulator = bgls.Simulator(
-        init_state,
-        bgls.state_vector_bitstring_probability,
-        cirq.protocols.act_on,
-    )
-    bgls_result = bgls_simulator.sample(ghz, repetitions=100, seed=12)
-    bgls_result_multiple = bgls_simulator.sample(
-        ghz_multiple, repetitions=100, seed=12
-    )
-    # converting to histogram for checking recovers original values since
-    # spread across multiple measurement keys
-    np.testing.assert_array_equal(
-        cirq.vis.get_state_histogram(bgls_result),
-        cirq.vis.get_state_histogram(bgls_result_multiple),
-    )
+#
+# def test_intermediate_measurements():
+#     # Intermediate measurement gates do not affect simulation
+#     q0, q1, q2 = cirq.LineQubit.range(3)
+#     ghz = cirq.Circuit(
+#         cirq.H(q0),
+#         cirq.CNOT(q0, q1),
+#         cirq.CNOT(q1, q2),
+#         cirq.measure([q0, q1, q2], key="result"),
+#     )
+#     ghz_intermediate = cirq.Circuit(
+#         cirq.H(q0),
+#         cirq.measure([q0, q2], key="result"),
+#         cirq.CNOT(q0, q1),
+#         cirq.measure([q0, q1, q2], key="result"),
+#         cirq.CNOT(q1, q2),
+#         cirq.measure([q0, q1, q2], key="result"),
+#     )
+#
+#     bgls_simulator = bgls.Simulator(
+#         cirq.StateVectorSimulationState(
+#             qubits=(q0, q1, q2), initial_state=0
+#         ),
+#         cirq.protocols.act_on,
+#         bgls.state_vector_bitstring_probability,
+#         seed=1,
+#     )
+#     bgls_result = bgls_simulator.sample(ghz, repetitions=100)
+#     bgls_result_intermediate = bgls_simulator.sample(
+#         ghz_intermediate, repetitions=100
+#     )
+#     assert bgls_result == bgls_result_intermediate
 
 
-def test_no_measurements():
-    # Sampling a circuit without final measurement raises a ValueError
-    q0, q1, q2 = cirq.LineQubit.range(3)
-    ghz = cirq.Circuit(cirq.H(q0), cirq.CNOT(q0, q1), cirq.CNOT(q0, q2))
-    init_state = cirq.StateVectorSimulationState(
-        qubits=(q0, q1, q2), initial_state=0
-    )
-    bgls_simulator = bgls.Simulator(
-        init_state,
-        bgls.state_vector_bitstring_probability,
-        cirq.protocols.act_on,
-    )
-    with pytest.raises(ValueError) as error_info:
-        bgls_result = bgls_simulator.sample(
-            ghz,
-            repetitions=100,
-        )
-
-
-def test_partial_measurements():
-    # Measuring only some final qubits yields the same distribution,
-    # but with bitstring int rep matching the subset of measured bits
-    q0, q1, q2 = cirq.LineQubit.range(3)
-    ghz = cirq.Circuit(
-        cirq.H(q0),
-        cirq.CNOT(q0, q1),
-        cirq.CNOT(q0, q2),
-        cirq.measure([q0, q2], key="result"),
-    )
-    init_state = cirq.StateVectorSimulationState(
-        qubits=(q0, q1, q2), initial_state=0
-    )
-    bgls_simulator = bgls.Simulator(
-        init_state,
-        bgls.state_vector_bitstring_probability,
-        cirq.protocols.act_on,
-    )
-    bgls_result = bgls_simulator.sample(
-        ghz,
-        repetitions=100,
-    )
-    # here because ghz, still only get low and high-end values with equal
-    # prob, but now 0 and 3 (ie 00 11) rather than 0 and 7 (000 111)
-    # can assert these are the only measured values
-    assert set(bgls_result.histogram(key="result")).issubset({0, 3})
-
-
-def test_density_state():
-    # Density matrix simulation yields the same result as state vector
-    q0, q1, q2 = cirq.LineQubit.range(3)
-    ghz = cirq.Circuit(
-        cirq.H(q0),
-        cirq.CNOT(q0, q1),
-        cirq.CNOT(q0, q2),
-        cirq.measure([q0, q1, q2], key="result"),
-    )
-    statevector_state = cirq.StateVectorSimulationState(
-        qubits=(q0, q1, q2), initial_state=0
-    )
-    statevector_simulator = bgls.Simulator(
-        statevector_state,
-        bgls.state_vector_bitstring_probability,
-        cirq.protocols.act_on,
-    )
-    statevector_result = statevector_simulator.sample(
-        ghz, repetitions=100, seed=3
-    )
-    density_state = cirq.DensityMatrixSimulationState(
-        qubits=(q0, q1, q2), initial_state=0
-    )
-    density_simulator = bgls.Simulator(
-        density_state,
-        bgls.state_vector_bitstring_probability,
-        cirq.protocols.act_on,
-    )
-    density_result = density_simulator.sample(ghz, repetitions=100, seed=3)
-    assert statevector_result == density_result
+# def test_multiple_measurements():
+#     # Distributing final measurement across multiple gates has same behavior
+#     # as a single measurement of all qubits
+#     q0, q1, q2 = cirq.LineQubit.range(3)
+#     ghz = cirq.Circuit(
+#         cirq.H(q0),
+#         cirq.CNOT(q0, q1),
+#         cirq.CNOT(q0, q2),
+#         cirq.measure([q0, q1, q2], key="result"),
+#     )
+#     ghz_multiple = cirq.Circuit(
+#         cirq.H(q0),
+#         cirq.CNOT(q0, q1),
+#         cirq.CNOT(q0, q2),
+#         cirq.measure([q0, q2], key="result1st"),
+#         cirq.measure([q1], key="result2nd"),
+#     )
+#     init_state = cirq.StateVectorSimulationState(
+#         qubits=(q0, q1, q2), initial_state=0
+#     )
+#     bgls_simulator = bgls.Simulator(
+#         init_state,
+#         bgls.state_vector_bitstring_probability,
+#         cirq.protocols.act_on,
+#     )
+#     bgls_result = bgls_simulator.sample(ghz, repetitions=100, seed=12)
+#     bgls_result_multiple = bgls_simulator.sample(
+#         ghz_multiple, repetitions=100, seed=12
+#     )
+#     # converting to histogram for checking recovers original values since
+#     # spread across multiple measurement keys
+#     np.testing.assert_array_equal(
+#         cirq.vis.get_state_histogram(bgls_result),
+#         cirq.vis.get_state_histogram(bgls_result_multiple),
+#     )
+#
+#
+# def test_no_measurements():
+#     # Sampling a circuit without final measurement raises a ValueError
+#     q0, q1, q2 = cirq.LineQubit.range(3)
+#     ghz = cirq.Circuit(cirq.H(q0), cirq.CNOT(q0, q1), cirq.CNOT(q0, q2))
+#     init_state = cirq.StateVectorSimulationState(
+#         qubits=(q0, q1, q2), initial_state=0
+#     )
+#     bgls_simulator = bgls.Simulator(
+#         init_state,
+#         bgls.state_vector_bitstring_probability,
+#         cirq.protocols.act_on,
+#     )
+#     with pytest.raises(ValueError) as error_info:
+#         bgls_result = bgls_simulator.sample(
+#             ghz,
+#             repetitions=100,
+#         )
+#
+#
+# def test_partial_measurements():
+#     # Measuring only some final qubits yields the same distribution,
+#     # but with bitstring int rep matching the subset of measured bits
+#     q0, q1, q2 = cirq.LineQubit.range(3)
+#     ghz = cirq.Circuit(
+#         cirq.H(q0),
+#         cirq.CNOT(q0, q1),
+#         cirq.CNOT(q0, q2),
+#         cirq.measure([q0, q2], key="result"),
+#     )
+#     init_state = cirq.StateVectorSimulationState(
+#         qubits=(q0, q1, q2), initial_state=0
+#     )
+#     bgls_simulator = bgls.Simulator(
+#         init_state,
+#         bgls.state_vector_bitstring_probability,
+#         cirq.protocols.act_on,
+#     )
+#     bgls_result = bgls_simulator.sample(
+#         ghz,
+#         repetitions=100,
+#     )
+#     # here because ghz, still only get low and high-end values with equal
+#     # prob, but now 0 and 3 (ie 00 11) rather than 0 and 7 (000 111)
+#     # can assert these are the only measured values
+#     assert set(bgls_result.histogram(key="result")).issubset({0, 3})
+#
+#
+# def test_density_state():
+#     # Density matrix simulation yields the same result as state vector
+#     q0, q1, q2 = cirq.LineQubit.range(3)
+#     ghz = cirq.Circuit(
+#         cirq.H(q0),
+#         cirq.CNOT(q0, q1),
+#         cirq.CNOT(q0, q2),
+#         cirq.measure([q0, q1, q2], key="result"),
+#     )
+#     statevector_state = cirq.StateVectorSimulationState(
+#         qubits=(q0, q1, q2), initial_state=0
+#     )
+#     statevector_simulator = bgls.Simulator(
+#         statevector_state,
+#         bgls.state_vector_bitstring_probability,
+#         cirq.protocols.act_on,
+#     )
+#     statevector_result = statevector_simulator.sample(
+#         ghz, repetitions=100, seed=3
+#     )
+#     density_state = cirq.DensityMatrixSimulationState(
+#         qubits=(q0, q1, q2), initial_state=0
+#     )
+#     density_simulator = bgls.Simulator(
+#         density_state,
+#         bgls.state_vector_bitstring_probability,
+#         cirq.protocols.act_on,
+#     )
+#     density_result = density_simulator.sample(ghz, repetitions=100, seed=3)
+#     assert statevector_result == density_result
