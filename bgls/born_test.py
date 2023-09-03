eb6dc4a00946261b6f1d028f988d098fa0dfda5b
Ryan LaRose <rlarose@umich.edu>
2023-09-02 21:24:26 -0400

    Format.

--- bgls/born_test.py
+++ bgls/born_test.py
@@ -20,27 +20,57 @@ import bgls
 
 
 def test_compute_probability_state_vector():
-    state = cirq.StateVectorSimulationState(qubits=cirq.LineQubit.range(2), initial_state=0b00)
+    state = cirq.StateVectorSimulationState(
+        qubits=cirq.LineQubit.range(2), initial_state=0b00
+    )
 
-    assert np.isclose(bgls.born.compute_probability_state_vector(state, "00"), 1.0)
-    assert np.isclose(bgls.born.compute_probability_state_vector(state, "01"), 0.0)
-    assert np.isclose(bgls.born.compute_probability_state_vector(state, "10"), 0.0)
-    assert np.isclose(bgls.born.compute_probability_state_vector(state, "11"), 0.0)
+    assert np.isclose(
+        bgls.born.compute_probability_state_vector(state, "00"), 1.0
+    )
+    assert np.isclose(
+        bgls.born.compute_probability_state_vector(state, "01"), 0.0
+    )
+    assert np.isclose(
+        bgls.born.compute_probability_state_vector(state, "10"), 0.0
+    )
+    assert np.isclose(
+        bgls.born.compute_probability_state_vector(state, "11"), 0.0
+    )
 
 
 def test_compute_probability_density_matrix():
-    state = cirq.DensityMatrixSimulationState(qubits=cirq.LineQubit.range(2), initial_state=0b01)
+    state = cirq.DensityMatrixSimulationState(
+        qubits=cirq.LineQubit.range(2), initial_state=0b01
+    )
 
-    assert np.isclose(bgls.born.compute_probability_density_matrix(state, "00"), 0.0)
-    assert np.isclose(bgls.born.compute_probability_density_matrix(state, "01"), 1.0)
-    assert np.isclose(bgls.born.compute_probability_density_matrix(state, "10"), 0.0)
-    assert np.isclose(bgls.born.compute_probability_density_matrix(state, "11"), 0.0)
+    assert np.isclose(
+        bgls.born.compute_probability_density_matrix(state, "00"), 0.0
+    )
+    assert np.isclose(
+        bgls.born.compute_probability_density_matrix(state, "01"), 1.0
+    )
+    assert np.isclose(
+        bgls.born.compute_probability_density_matrix(state, "10"), 0.0
+    )
+    assert np.isclose(
+        bgls.born.compute_probability_density_matrix(state, "11"), 0.0
+    )
 
 
 def test_compute_probability_stabilizer_state():
-    state = cirq.StabilizerChFormSimulationState(qubits=cirq.LineQubit.range(2), initial_state=0b10)
+    state = cirq.StabilizerChFormSimulationState(
+        qubits=cirq.LineQubit.range(2), initial_state=0b10
+    )
 
-    assert np.isclose(bgls.born.compute_probability_stabilizer_state(state, "00"), 0.0)
-    assert np.isclose(bgls.born.compute_probability_stabilizer_state(state, "01"), 0.0)
-    assert np.isclose(bgls.born.compute_probability_stabilizer_state(state, "10"), 1.0)
-    assert np.isclose(bgls.born.compute_probability_stabilizer_state(state, "11"), 0.0)
+    assert np.isclose(
+        bgls.born.compute_probability_stabilizer_state(state, "00"), 0.0
+    )
+    assert np.isclose(
+        bgls.born.compute_probability_stabilizer_state(state, "01"), 0.0
+    )
+    assert np.isclose(
+        bgls.born.compute_probability_stabilizer_state(state, "10"), 1.0
+    )
+    assert np.isclose(
+        bgls.born.compute_probability_stabilizer_state(state, "11"), 0.0
+    )
